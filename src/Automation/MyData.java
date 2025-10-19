42b870dc381d887ce04c9de60d7fe5aedc8a37ef
Hala Amayreh <halaamayreh72@gmail.com>
2025-10-19 21:53:49 +0300

    Store2

---
+++ src/Automation/MyData.java
@@ -0,0 +1,38 @@
+package Automation;
+
+import java.util.Random;
+
+public class MyData
+
+{
+	 boolean WantToSubscribe ;
+
+	Random rand = new Random();
+	
+	String [] firstNames = {"Shatha" , "zainab" , "ayham" , "abdulrahman","ammar" , "sana"};
+	String [] lastNames = {"mohammad","Mahmoud","ali","omar","noor","rami"};
+	String domain = "@gmail.com";
+	int randomNumberForTheEmail = rand.nextInt(9583);
+	int randomFirstNameIndex = rand.nextInt(firstNames.length);
+	int randomLastNameIndex = rand.nextInt(lastNames.length);
+	
+	String TheFirstName = firstNames[randomFirstNameIndex];
+	String TheLastNames = lastNames[randomLastNameIndex];
+	String TheEmail= TheFirstName+TheLastNames+randomNumberForTheEmail+domain;
+	
+	String telephoneNumber = "0792682544";
+	String theFax = "232445";
+	String theAdressOne = "Amman-Jordan";
+	
+	String LOGINNAME = TheFirstName+TheLastNames+randomNumberForTheEmail;
+	String PostalCode = "374327";
+	String Password = "P@$$word";
+	
+	int TheSelectIndex = rand.nextInt(1,10);
+	
+	 String ExpectedTextForSignUp = "YOUR ACCOUNT HAS BEEN CREATED!";
+
+	 
+      
+
+}
---
+++ src/Automation/MyTestCases.java
@@ -0,0 +1,120 @@
+package Automation;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyTestCases extends MyData
+
+{
+	WebDriver driver = new EdgeDriver();
+	String theURL ="https://automationteststore.com/";
+    String SignUpPage ="https://automationteststore.com/index.php?rt=account/create";
+	
+	@BeforeTest
+	public void MySetup ()
+	
+	{
+          driver.get(theURL);
+        // Set an implicit wait time (wait up to 5 seconds when locating elements)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        
+        // Maximize the browser window
+        driver.manage().window().maximize();
+        
+        
+		     
+     }
+		
+		
+		
+	
+	
+	
+	
+	@Test (priority=1)
+	public void SignUp() throws InterruptedException
+	
+	{
+		 driver.navigate().to(SignUpPage);
+		 WebElement FirstName = driver.findElement(By.id("AccountFrm_firstname"));
+		 WebElement LastName = driver.findElement(By.id("AccountFrm_lastname"));
+		 WebElement Email = driver.findElement(By.id("AccountFrm_email"));
+         WebElement TelephoneNumber = driver.findElement(By.id("AccountFrm_telephone"));
+         WebElement TheFax = driver.findElement(By.id("AccountFrm_fax"));
+		 WebElement AddressOne = driver.findElement(By.id("AccountFrm_address_1"));
+		 WebElement theCountry = driver.findElement(By.id("AccountFrm_country_id"));
+		 WebElement TheState = driver.findElement(By.id("AccountFrm_zone_id"));
+		 WebElement ThePostalCode = driver.findElement(By.id("AccountFrm_postcode"));
+		 WebElement LoginName = driver.findElement(By.id("AccountFrm_loginname"));
+		WebElement ThePassword = driver.findElement(By.id("AccountFrm_password"));
+		WebElement TheConfirmPassword = driver.findElement(By.id("AccountFrm_confirm"));
+		WebElement TheAgreeBox = driver.findElement(By.id("AccountFrm_agree"));
+		WebElement SubscribeYes = driver.findElement(By.id("AccountFrm_newsletter1"));
+		WebElement SubscribeNo = driver.findElement(By.name("newsletter"));
+		if (WantToSubscribe=true) 
+			 
+		 {
+			SubscribeYes.click();
+		 }else {SubscribeNo.click();}
+		Thread.sleep(2000);
+		
+		WebElement Continue = driver.findElement(By.cssSelector(".btn.btn-orange.pull-right.lock-on-click"));
+
+
+		 Select mySelectElementForCountry = new Select(theCountry);
+		 mySelectElementForCountry.selectByIndex(108);
+          Thread.sleep(2000);
+		 Select mySelectElementForState = new Select(TheState);
+		 mySelectElementForState.selectByIndex(TheSelectIndex); 
+		 
+		 List <WebElement> AlltheStates = TheState.findElements(By.tagName("option"));
+		 String theCity = AlltheStates.get(TheSelectIndex).getText();
+		 WebElement TheCityInput = driver.findElement(By.id("AccountFrm_city"));
+
+		 
+		 //actions
+		 FirstName.sendKeys(TheFirstName);
+		 LastName.sendKeys(TheLastNames);
+		 Email.sendKeys(TheEmail);
+		 TelephoneNumber.sendKeys(telephoneNumber);
+		 TheFax.sendKeys(theFax);
+		 AddressOne.sendKeys(theAdressOne);
+		 TheCityInput.sendKeys(theCity);
+		 ThePostalCode.sendKeys(PostalCode);
+		 LoginName.sendKeys(LOGINNAME);
+		 ThePassword.sendKeys(Password);
+		 TheConfirmPassword.sendKeys(Password);
+		 TheAgreeBox.click();
+		 Thread.sleep(2000);
+		 Continue.click();
+		 
+		
+		 String ActualTextForSignUp = driver.findElement(By.className("maintext")).getText();
+		 
+		 Assert.assertEquals(ActualTextForSignUp, ExpectedTextForSignUp);
+
+		
+	}
+
+    
+
+    @AfterTest
+    public void AfterMyTest() 
+    {
+    	
+     // driver.close();
+    }
+        
+ 
+
+}
